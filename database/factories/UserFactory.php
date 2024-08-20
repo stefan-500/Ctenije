6af5abeec86fbb4256d1be698b5dfd10121332a8
stefan-500 <wqygstefan900zwqi@gmail.com>
2024-08-20 20:31:03 +0200

    first commit

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'ime' => fake()->firstName(),
+            'prezime' => fake()->lastName(),
+            'email' => fake()->unique()->email(),
+            'email_verified_at' => now(),
+            'password' => static::$password ??= Hash::make('password'),
+            'ovlascenje' => 'Korisnik',
+            'tel' => fake()->e164PhoneNumber(),
+            'adresa' => fake()->streetAddress(),
+            'informacije' => 0,
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
